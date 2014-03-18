b675c08444121559db1b7f6b18f5b755436bc55c
MiroG <miroslaw.golda@gmail.com>
2014-03-18 21:07:37 +0100

    simplification of the digit building process

--- src/main/scala/pl/allegro/funkata/OCRReader.scala
+++ src/main/scala/pl/allegro/funkata/OCRReader.scala
@@ -1,41 +1,37 @@
 package pl.allegro.funkata
 import scala.io.{BufferedSource, Source}
-import java.net.URI
+import scala.annotation.tailrec
 
 class OCRReader {
-  
-  type CharLine = List[Char]
-  
+
+  type StringLine = List[String]
+
 
   def readInput(fileName: String): String = {
-    val digitals: List[List[CharLine]] = readFile(fileName) map {splitToCharLines(_)}
-    val output = parseInputDigitals(digitals)  
-    invalidate(output) 
+    val digitals: List[StringLine] = readFile(fileName) map {splitToStringLine(_)}
+    val output = parseInputDigitals(digitals)
+    invalidate(output)
   }
 
 
-  def parseInputDigitals(chars: List[List[CharLine]]): String = {
-    (chars(0) zip chars(1) zip chars(2) zip chars(3)) map {
-      case (((a, b), c), d) =>
-        List(a, b, c, d) map (_.mkString) mkString (sys.props("line.separator"))
-    } map digitalToDigit mkString
-  }
+  def parseInputDigitals(lines: List[StringLine]): String =
+    (lines(0) zip lines(1) zip lines(2) zip lines(3)).map {
+      case (((a, b), c), d) => List(a, b, c, d).mkString(sys.props("line.separator"))
+    }.map(digitalToDigit).mkString
 
-   
 
   def crCheck(output: String): Boolean = {
     val crcSum: Int = output.map(_.asDigit).reverse.zipWithIndex.map {
       case (x, index) =>  x * (index + 1)
     }.sum
-    crcSum % 11 == 0   
+    crcSum % 11 == 0
   }
-  
+
   def invalidate (output: String):String = output match {
-      case invalid if invalid.contains("?") => invalid + " ILL"
-      case errornous if !crCheck(output) => errornous + " ERR"
-      case s => s
-    }
-  
+    case invalid if invalid.contains("?") => invalid + " ILL"
+    case errornous if !crCheck(output) => errornous + " ERR"
+    case s => s
+  }
 
   def digitalToDigit: (String) => String = {
 
@@ -60,12 +56,10 @@ class OCRReader {
     input
   }
 
-  def splitToCharLines (text: String, acc: List[CharLine] = List.empty[CharLine]): List[CharLine] =
+  @tailrec
+  private[this] def splitToStringLine (text: String, acc: StringLine = List()): StringLine =
     text match {
       case "" => acc
-      case s => splitToCharLines(s.drop(3), acc :+ s.take(3).toCharArray.toList)
-     }
-  
-
-
+      case s => splitToStringLine(s.drop(3), acc :+ s.take(3))
+    }
 }
